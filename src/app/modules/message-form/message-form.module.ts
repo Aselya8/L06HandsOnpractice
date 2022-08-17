6c4e58aab0bef3ba293f1a8ec117538c76d942b3
Asel Atakeev <aselya.atakeeva@gmail.com>
2022-08-17 08:53:02 -0700

    first commit

---
+++ src/app/modules/message-form/message-form.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//added import
+import { FormsModule } from '@angular/forms';
+
+import { MessageFormComponent } from './components/message-form/message-form.component';
+
+@NgModule({
+  imports: [
+    CommonModule, FormsModule
+  ],
+  declarations: [MessageFormComponent],
+  exports: [MessageFormComponent]
+})
+export class MessageFormModule { }
